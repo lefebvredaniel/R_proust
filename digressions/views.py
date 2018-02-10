36b760dbf4bd19294d768751fa6434bfee4d6481
dle2002 <daniellefebvre@24dcaea7d349>
2018-02-10 10:29:14 +0000

    bascule de prod de pythonanywhere sur github :wq

--- digressions/views.py
+++ digressions/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404
 
 from django.http import HttpResponseRedirect, HttpResponse
 from django.db import models
@@ -14,7 +14,7 @@ from django.urls import reverse
 ##                              PAGE ACCUEIL
 def index(request):
 
- 
+
 
   #  etiquettes_list = Etiquettes.objects.all().order_by ('etiquettes_nom')
     etiquettes_list=Etiquettes.objects.annotate(nb=Count('r_extraits_etiquettes')).order_by('-nb')[:10]
@@ -22,33 +22,33 @@ def index(request):
     dico={}
     ##   EE va représenter une étiquette tirée de etiquettes_list ex. [<Etiquettes: songe>]
     for EE in etiquettes_list:
-                      
+
     ##on ajoute au dictionnaire "dico" le nombre d'occurrences de l'étiquette EE {<Etiquettes: songe>: 1, <Etiquettes: jalousie>: 1,
     ##      <Etiquettes: amour>: 2 etc.}
-        dico[EE]=EE.nb       
+        dico[EE]=EE.nb
     context = {'dico':dico}
 
     return render(request, 'digressions/index.html', context)
 ##                              PAGE CONTENU (extrait)
 def contenu(request, titre_id):
-    
- 
-    
+
+
+
     titre = get_object_or_404(Extraits, pk=titre_id)
-   
-    
+
+
     return render(request, 'digressions/contenu.html', {'titre': titre})
 
 def apropos(request):
     return render(request, 'digressions/apropos.html',{})
 
 def detail(request, etiq_id):
-   
-   
+
+
     selection_list= R_Extraits_Etiquettes.objects.filter(etiquettes_id_id=etiq_id)
     etiquettes_list = Etiquettes.objects.filter(id=etiq_id)
-    
- 
+
+
     context={'etiquettes_list':selection_list,'nom_etiquette':etiquettes_list}
 
     return render(request, 'digressions/detail.html',context)
@@ -58,9 +58,9 @@ def poursyretrouver(request):
     etiquettes_list=Etiquettes.objects.annotate(nb=Count('r_extraits_etiquettes')).order_by('etiquettes_nom')
  #   V=Etiquettes.objects.get(id=2).etiquettes_nom
  #   print(V[0])
-    
- 
- 
+
+
+
     PL=''
     dico={}
     ##   EE va représenter une étiquette tirée de etiquettes_list ex. [<Etiquettes: songe>]
@@ -68,16 +68,16 @@ def poursyretrouver(request):
 
         premiereLettre=Etiquettes.objects.get(id=EE.id).etiquettes_nom
         print(premiereLettre[0])
-        
+
         if (PL!=premiereLettre[0]):
-            
+
             A=str(premiereLettre[0])
-                  
+
             dico[A.upper]=''
-            
-        
-            
-                      
+
+
+
+
     ##on ajoute au dictionnaire "dico" le nombre d'occurrences de l'étiquette EE {<Etiquettes: songe>: 1, <Etiquettes: jalousie>: 1,
     ##      <Etiquettes: amour>: 2 etc.}
         B=str(EE.nb)
--- site_proust/urls.py
+++ site_proust/urls.py
@@ -19,13 +19,13 @@ from django.conf import settings
 from django.conf.urls import url
 
 urlpatterns= [
- 
-    path('digressions/', include('digressions.urls')),
+
+    path('', include('digressions.urls')),
     path('admin/', admin.site.urls)
-    
+
 ]
-if settings.DEBUG:
-   import debug_toolbar
-   urlpatterns = [
-        url(r'^__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
+#if settings.DEBUG:
+#   import debug_toolbar
+  # urlpatterns = [
+#        url(r'^__debug__/', include(debug_toolbar.urls)),
+ #   ] + urlpatterns
