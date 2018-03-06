1a09fd3b561e3fb5cef5d567d20357cb6b8ad558
dle2002 <daniellefebvre@24dcaea7d349>
2018-03-06 20:50:39 +0000

    alignement#

--- digressions/models.py
+++ digressions/models.py
@@ -8,48 +8,51 @@ class Livre(models.Model):
        livre_titre = models.CharField(max_length=50)
        def __str__(self):
             return self.livre_titre
-    
+
+       class meta:
+            ordering =['id']
+
 
 class Extraits(models.Model):
- 
+
     extraits_content = models.CharField(max_length=2000)
     extraits_titre = models.CharField(max_length=50)
-    extraits_livre = models.ForeignKey (Livre, 
+    extraits_livre = models.ForeignKey (Livre,
         on_delete=models.CASCADE)
- 
+
     def __str__(self):
-         return self.extraits_titre       
+         return self.extraits_titre
     pub_date = models.DateTimeField('date published')
-    
-    
+
+
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Etiquettes(models.Model):
- 
+
     etiquettes_nom = models.CharField(max_length=50)
-    
+
     class meta:
         ordering = ['etiquettes_nom']
- 
+
     def __str__(self):
         return self.etiquettes_nom
-    
+
 class Commentaires(models.Model):
- 
-    author = models.ForeignKey (User, 
+
+    author = models.ForeignKey (User,
         on_delete=models.CASCADE)
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
     titre = models.ForeignKey (Extraits, null=True,on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.body 
+        return self.body
 # Create your models here.
 class R_Extraits_Etiquettes(models.Model):
    extraits_id = models.ForeignKey(Extraits, on_delete=models.CASCADE)
    etiquettes_id = models.ForeignKey(Etiquettes, on_delete=models.CASCADE)
-       
 
-    
+
+
